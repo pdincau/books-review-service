0bc5f752550153f93f35c2bcb6c1eaaee3c096d2
Paolo D'Incau <paolo.dincau@xpeppers.com>
2016-06-30 13:56:00 +0200

    adding command

---
+++ src/main/java/CommandGetViews.java
@@ -0,0 +1,24 @@
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommandGetViews extends HystrixCommand<View> {
+
+    private final ViewRepository repository;
+    private final String isbn;
+
+    static final Logger LOG = LoggerFactory.getLogger(CommandGetViews.class);
+
+    public CommandGetViews(ViewRepository repository, String isbn) {
+        super(HystrixCommandGroupKey.Factory.asKey("BookReviewGroup"));
+        this.repository = repository;
+        this.isbn = isbn;
+    }
+
+    @Override
+    protected View run() throws Exception {
+        LOG.info("Invoking command to retrieve view");
+        return repository.findBy(isbn);
+    }
+}
